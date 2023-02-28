6a6489b2ff3b302079b563a05b5f7b4cede42e8f
Win Htut <winhtutonline@gmail.com>
2023-02-28 22:10:48 +0630

    error_fixed

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+// Created by National Cyber City on 2/6/2023.
+//
+
+
+#include "stdio.h"
+#include "zoom_online_bank.h"
+
+int main(){
+
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+
+    return 0;
+}
+
